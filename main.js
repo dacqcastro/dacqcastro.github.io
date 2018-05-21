d4cba38909fbd9a9f5c659a4c92f274913ac77e4
Diego Castro <37378645+dacqcastro@users.noreply.github.com>
2018-05-20 23:14:45 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const menuButton = document.querySelector(".small-screen-nav");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuScreen = document.querySelector(".mobile-menu-screen");
+const closeBtn = document.querySelector("#close-menu");
+
+menuButton.addEventListener("click", function(){
+  mobileMenuScreen.style.width = "20em";
+  mobileMenu.style.bottom = "0";
+  setTimeout(function(){
+    closeBtn.style.display = "block";
+  },500);
+});
+
+closeBtn.addEventListener("click", function(){
+  mobileMenuScreen.style.width = "0";
+  mobileMenu.style.bottom = "100%";
+  closeBtn.style.display = "none";
+});
+
+window.addEventListener("click", function(e){
+  if(e.target == mobileMenu){
+    mobileMenuScreen.style.width = "0";
+    mobileMenu.style.bottom = "100%";
+    closeBtn.style.display = "none";
+  }
+});
